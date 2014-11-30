8709fee9ace6f3167cf25351d87313059af6c455
ltfschoen <ltfschoen@gmail.com>
2014-11-30 21:16:05 +1100

    Finished Countdown problem

--- .dist-buildwrapper/src/Chapter2/Section2/Countdown.hs
+++ .dist-buildwrapper/src/Chapter2/Section2/Countdown.hs
@@ -38,6 +38,14 @@ module Countdown where
 -- Combinatorial functions
 -----------------------
 
+{- Subsets Ordered Lists that comprise the input List
+   Test with:
+    subs [0]       ==> [[],[0]]
+    subs [1,2]     ==> [[],[2],[1],[1,2]]
+    subs [1,2,3]   ==> [[],[3],[2],[2,3],[1],[1,3],[1,2],[1,2,3]]
+    subs [1,2,3,4] ==> [[],[4],[3],[3,4],[2],[2,4],[2,3],[2,3,4],[1],[1,4],[1,3],[1,3,4],[1,2],[1,2,4],[1,2,3],[1,2,3,4]]
+-}
+
   subs                          :: [a] -> [[a]]
   subs []                       =  [[]]
   subs (x:xs)                   =  yss ++ map (x:) yss
@@ -46,14 +54,55 @@ module Countdown where
   interleave                    :: a -> [a] -> [[a]]
   interleave x []               =  [[x]]
   interleave x (y:ys)           =  (x:y:ys) : map (y:) (interleave x ys)
- 
+
+{- Permutations of a List (different ways the existing List can be ordered, including its original Order)
+   Test with:
+
+    perms [1,2]   ==> [[1,2],[2,1]]
+    perms [1,2,3] ==> [[1,2,3],[2,1,3],[2,3,1],[1,3,2],[3,1,2],[3,2,1]]
+-}
+
   perms                         :: [a] -> [[a]]
   perms []                      =  [[]]
   perms (x:xs)                  =  concat (map (interleave x) (perms xs))
  
+{- Ex. 0
+  Choose a possible definition of the function choices :: [a] -> [[a]], 
+  that returns all choices from a list, which are given by all possible 
+  ways of selecting zero or more elements in any order.
+  Note: the definition of "subs" and "perms" can be found in the given template, 
+  make sure that you play around with these functions in GHCi to understand 
+  how they work before you attempt to compose them together to implement "choices"
+-}
+ 
+{- WRONG Test with:
+  choices [] ==> [[]]
+  choices [1] ==> [[1],[1,1]]
+  choices [1,2] ==> [[1,2],[2,1],[2,1,2],[2,2,1],[1,1,2],[1,2,1],[1,2,1,2],[1,2,2,1]]
+     
   choices                       :: [a] -> [[a]]
-  choices                       =  undefined
-
+  choices xs                    = [ys ++ zs | ys <- subs xs, zs <- perms xs]
+-}
+{- WRONG - incorrect type
+  choices                       :: [a] -> [[a]]
+  choices xs                    = concat [zs | ys <- subs xs, zs <- perms ys]
+-}
+{- RIGHT Test with: 
+  choices [] ==> [[]]
+  choices [1] ==> [[],[1]]
+  choices [1,2] ==> [[],[2],[1],[1,2],[2,1]]
+-}  
+  choices                       :: [a] -> [[a]]
+  choices xs                    = [zs | ys <- subs xs, zs <- perms ys]
+{--}
+{- WRONG Test with:
+  choices [] ==> [[]]
+  choices [1] ==> [[],[1]]
+  choices [1,2] ==> [[],[2],[1],[1,2],[],[1],[2],[2,1]]
+  
+  choices                       :: [a] -> [[a]]
+  choices xs                    = [zs | ys <- perms xs, zs <- subs ys] 
+-}
 -- Formalising the problem
 -----------------------
 
@@ -63,8 +112,43 @@ module Countdown where
 -- Brute force solution
 --------------------
 
-  split                         :: [a] -> [([a],[a])]
-  split                         =  undefined
+{- Ex. 3
+   Choose a correct implementation of the function 
+   
+   split :: [a] -> [([a],[a])] 
+   
+   seen in the lecture, that returns all possible ways of 
+   splitting a list into two non-empty lists that append to 
+   give the original list.
+   
+   For additional understanding, try to test this function using quickcheck.
+-}
+{-
+  split :: [a] -> [([a], [a])]
+  split [] = []
+  split [x] = [x]
+  split (x : xs) = [([x] : (ls ++ rs)) | (ls, rs) <- split xs]
+  
+  split :: [a] -> [([a], [a])]
+  split [] = []
+  split (x : xs) = ([x], xs) : (split xs)
+  
+  split :: [a] -> [([a], [a])]
+  split [] = []
+  split (x : xs) = [(x : ls, rs) | (ls, rs) <- split xs]
+-}
+ 
+{- RIGHT
+  Test with:
+  split [1,3,7,10,25,50] ==> 
+  [([1],[3,7,10,25,50]),([1,3],[7,10,25,50]),([1,3,7],[10,25,50]),([1,3,7,10],[25,50]),([1,3,7,10,25],[50])]
+-}
+  
+  split :: [a] -> [([a], [a])]
+  split [] = []
+  split [_] = []
+  split (x : xs) = ([x], xs) : [(x : ls, rs) | (ls, rs) <- split xs]
+  
   
   exprs                         :: [Int] -> [Expr]
   exprs []                      =  []
@@ -118,7 +202,7 @@ module Countdown where
   
   results'                      :: [Int] -> [Result]
   results' []                   =  []
-  results' [n]                  =  [(Val n,n) | n 0]
+  results' [n]                  =  [(Val n,n) | n > 0]
   results' ns                   =  [res | (ls,rs) <- split ns
                                         , lx      <- results' ls
                                         , ry      <- results' rs
@@ -189,3 +273,95 @@ module Countdown where
                                       putStr "Enter the target number : "
                                       n  <- readLn
                                       display (solutions'' ns n)
+{- Ex. 1
+   Choose the correct implementation of a function 
+   
+   removeone :: Eq a => a -> [a] -> [a]
+  
+   that removes the first occurence of a given element from a list.
+-}
+{- WRONG - y not in scope
+  removeone :: Eq a => a -> [a] -> [a]
+  removeone x [] = [x]
+  removeone x ys
+    | x == head ys = ys
+    | otherwise = y : removeone x ys
+-}
+{- WRONG 
+  Tested with: 
+    removeone 2 [1,2,3,4,5]
+    [2,1,2,3,4]
+  Incorrectly remove last element and added given element at start  
+  
+  removeone :: Eq a => a -> [a] -> [a]
+  removeone x [] = []
+  removeone x (y : ys)
+    | x == y = ys
+    | otherwise = x : removeone y ys
+-}
+{- WRONG 
+  removeone :: Eq a => a -> [a] -> [a]
+  removeone x [] = []
+  removeone x ys
+    | x == head ys = ys
+    | otherwise = removeone x ys
+-}
+{- RIGHT
+  Test with: 
+    removeone 3 [2,3,5,1,5,3]
+    [2,5,1,5,3]
+-}    
+  removeone :: Eq a => a -> [a] -> [a]
+  removeone x [] = []
+  removeone x (y : ys)
+    | x == y = ys
+    | otherwise = y : removeone x ys    
+
+
+{- Ex. 2
+   Choose a correct implementation of the function 
+   
+   isChoice :: Eq a => [a] -> [a] -> Bool 
+   
+   that decides whether one list is chosen from another. 
+   In other words, "isChoice xs ys" checks whether all elements 
+   in "xs" are present in "ys".
+-}
+{- RIGHT
+  isChoice :: Eq a => [a] -> [a] -> Bool 
+  
+  isChoice [] _ = True
+  isChoice (x : xs) [] = False
+  isChoice (x : xs) ys = elem x ys && isChoice xs (removeone x ys)
+-}
+{- WRONG
+  isChoice :: Eq a => [a] -> [a] -> Bool 
+
+  isChoice [] _ = False
+  isChoice (x : xs) [] = True
+  isChoice (x : xs) (y : ys) 
+    = elem y xs && isChoice xs (removeone x ys)
+-}
+
+{- WRONG
+
+  isChoice :: Eq a => [a] -> [a] -> Bool 
+
+  isChoice [] _ = True
+  isChoice xs [] = True
+  isChoice xs ys
+    = elem (head xs) ys && isChoice xs (removeone (head y) ys)
+-}
+{- WRONG
+  Test with:
+  isChoice [1..2] [] ==> False
+  isChoice [1, 2, 3] [3, 2, 1] ==> True
+  
+  isChoice :: Eq a => [a] -> [a] -> Bool 
+
+  isChoice [] _ = True
+  isChoice (x : xs) [] = False
+  isChoice (x : xs) ys
+    = elem x ys && isChoice (removeone x xs) ys
+-}
+
